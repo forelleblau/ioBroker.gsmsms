05cdb3b35a537fc7082fbba9613d6bcc48cd2b9c
forelleblau <44144152+forelleblau@users.noreply.github.com>
2022-11-26 23:13:43 +0100

    Update blockly.js

--- admin/blockly.js
+++ admin/blockly.js
@@ -170,10 +170,10 @@ Blockly.JavaScript['gsmsms'] = function(block) {
   var dropdown_instance = block.getFieldValue('INSTANCE');
   var value_message = Blockly.JavaScript.valueToCode(block, 'MESSAGE', Blockly.JavaScript.ORDER_ATOMIC);
   var value_number = Blockly.JavaScript.valueToCode(block, 'RECIPIENT', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_alert = Blockly.JavaScript.valueToCode(block, 'ALERT', Blockly.JavaScript.ORDER_ATOMIC);
-
+  //var value_alert = Blockly.JavaScript.valueToCode(block, 'ALERT', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_alert = block.getFieldValue('ALERT');
   //return
-  this.log.debug('sendTo("gsmsms' + dropdown_instance + '", "send", {text:"' +
-    value_message + '",recipient:"' + value_number + '", alert:' + value_alert +
+  this.log.debug('sendTo("gsmsms' + dropdown_instance + '", "send", {text:' +
+    value_message + ',recipient:' + value_number + ', alert:' + value_alert +
     '});\n');
 };
